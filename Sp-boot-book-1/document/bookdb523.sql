6778003a2bdd1fc798c5a78f3fad6084e6a6965a
hykim6856 <hykim6856@naver.com>
2024-05-31 15:51:50 +0900

    backup

---
+++ Sp-boot-book-1/document/bookdb523.sql
@@ -0,0 +1,8 @@
+-- bookdb
+
+create database bookdb;
+USE bookDB;
+SHOW TABLES;
+DESC tbl_books;
+DROP table tbl_books;
+select * from tbl_books;
---
+++ Sp-boot-book-1/src/main/java/com/callor/book/dao/BookDao.java
@@ -0,0 +1,9 @@
+package com.callor.book.dao;
+
+import com.callor.book.models.BookVO;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookDao extends JpaRepository<BookVO,String> {
+
+
+}
